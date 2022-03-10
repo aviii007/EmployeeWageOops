3e57e3dbce8f466f67f81472b6308baf81793d64
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-11 01:02:56 +0530

    Adding the Daily Employee Wage calculation.

--- src/com/java/employeeWageDay8/OopsEmployeeWage.java
+++ src/com/java/employeeWageDay8/OopsEmployeeWage.java
@@ -11,11 +11,36 @@ public class OopsEmployeeWage {
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
+            System.out.println(" ");
         }
+    }
+
+    //UC2: Calculating Daily Employee Wage
+    public void dailyWage() {
+        // Check the employee is present or absent
+        int IS_FULL_TIME = 1;
+        // Calculating daily employee wage
+        final int empWagePerHrs = 20;
+        int empHrs = 0;
+        int empWage = 0;
+
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == IS_FULL_TIME) {
+            empHrs = 8;
+            System.out.println("Employee is Present");
+        } else {
+
+            empHrs = 0;
+            System.out.println("Employee is Absent");
+        }
+        empWage = empHrs * empWagePerHrs;
+        System.out.println("The Daily Wage of the Employee is :" + " " + empWage);
+        System.out.println(" ");
     }
 
     public static void main(String[] args) {
         OopsEmployeeWage object = new OopsEmployeeWage();
         object.welcome();
+        object.dailyWage();
     }
 }
