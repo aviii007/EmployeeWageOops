aa5dbaba250240de92cf1dd5ad6ff08088a3d833
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-11 00:48:38 +0530

    Adding the Welcome message.

---
+++ src/com/java/employeeWageDay8/OopsEmployeeWage.java
@@ -0,0 +1,11 @@
+package com.java.employeeWageDay8;
+
+public class OopsEmployeeWage {
+    public void welcome() {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+    public static void main(String[] args) {
+        OopsEmployeeWage object = new OopsEmployeeWage();
+        object.welcome();
+    }
+}
